6ba4ca5906b41c8bb69308cae6b01aebf85b1121
Shaun Alexander <shaun@sierraalpha.co.nz>
2020-08-30 05:37:44 +0000

    Scripted auto-commit on change (2020-08-30 05:37:44) by gitwatch.sh

--- kaianga/.kaianga/start_repo_watch_service.py
+++ kaianga/.kaianga/start_repo_watch_service.py
@@ -5,8 +5,29 @@ import toml
 
 repos = toml.load("repos-to-watch.toml")
 
-for repo in repos:
-   
+def main(raw_args=None):
 
-systemctl --user --now enable \
-          gitwatch@$(systemd-escape "'-r url/to/repository' '-b branch' /path/to/folder").service
+    parser = argparse.ArgumentParser(
+        description=("Get the relevant info to execute a "
+                     "Docker Build command that supports build secrets")
+    )
+
+    parser.add_argument(
+        "-d",
+        "--leave-dirty",
+        action="store_true",
+        default=False,
+        help=("If set this will leave the password files "
+              "in the directory when finished")
+    )
+
+    args = parser.parse_args(raw_args)
+
+    for repo in repos:
+
+
+    systemctl --user --now enable \
+              gitwatch@$(systemd-escape "'-r url/to/repository' '-b branch' /path/to/folder").service
+
+if __name__ == "__main__":
+    main()
