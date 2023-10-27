c3149d043ddb665a1a169e15f7da56ab31c81843
Shaun Alexander <shaun@sierraalpha.co.nz>
2023-10-27 14:43:02 +1300

    Pull across default envs to fish and set to default shell

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,11 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    echo "Sourcing envs from bash"
-    export (/bin/bash -c "source $HOME/.bash_profile && env") >> /dev/null
+    rbenv init - fish | source
+    export PATH="/usr/local/opt/libpq/bin:$PATH"
+    export JAVA_HOME="/Library/Java/JavaVirtualMachines/zulu-11.jdk/Contents/Home"
+    export ANDROID_HOME="$HOME/Library/Android/sdk"
+    export PATH="$PATH:$ANDROID_HOME/emulator"
+    export PATH="$PATH:$ANDROID_HOME/platform-tools"
+    export REACT_EDITOR=emacsclient
 end
 
