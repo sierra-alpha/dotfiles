83030e443d15f9451045d3382222c87bb1bd596a
Shaun Alexander <shaun@sierraalpha.co.nz>
2024-03-19 10:28:16 +1300

    Add swift and coffeescript modes to spacemacs
    Move java home in fish config, need to think about how to
    apply cross platform configs

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     rbenv init - fish | source
     export PATH="/usr/local/opt/libpq/bin:$PATH"
-    export JAVA_HOME="/usr/local/Cellar/openjdk@17/17.0.9/libexec/openjdk.jdk/Contents/Home"
+    export JAVA_HOME="/usr/local/opt/openjdk@17/bin/java"
     export ANDROID_HOME="$HOME/Library/Android/sdk"
     export PATH="$PATH:$ANDROID_HOME/emulator"
     export PATH="$PATH:$ANDROID_HOME/platform-tools"
