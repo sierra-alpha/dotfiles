f7c58072b6729e03c125b0bdf773f90519032458
Shaun Alexander <shaun@sierraalpha.co.nz>
2024-10-02 15:29:26 +1300

    wip
    Revert "wip"
    
    This reverts commit ae3c324218faaa82b97bbf28d65b697a9eca9a42.
    
    Revert "Revert "wip""
    
    This reverts commit 2706ba9df4021e753f76c8bd4fdff2cd74a4a380.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
+   # I like vi bindings 
+   fish_vi_key_bindings
+
    # Nothing
    test -e ~/.work_fish.config && source ~/.work_fish.config
 end
-
-
-alias assume="source (brew --prefix)/bin/assume.fish"
